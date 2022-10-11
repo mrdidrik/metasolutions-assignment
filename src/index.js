36102413056d1e907c0b5a9f3810d74cb64167f6
Eco Sys <didrikgauffin@gmail.com>
2022-10-11 03:47:09 +0200

    restructring and style

--- src/index.js
+++ src/index.js
@@ -2,36 +2,50 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 import { EntryStore } from '@entryscape/entrystore-js';
 
-import App from './App';
+const baseURI = "https://recruit.entryscape.net/store";
+const context = "1";
+var entrystore = new EntryStore(baseURI);
 
-const title = 'Assignment Metasolutions';
-
-// ReactDOM.render(
-//   <App title={title} />,
-//   document.getElementById('app')
-// );
-
-// module.hot.accept();
-
-
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
 
 function Artist(props) {
   const [givenName, setGivenName] = useState();
   const [familyName, setFamilyName] = useState();
 
+  // useEffect(() => {
+  //   var artistEntryId = entrystore.getEntryId(props.resourceURI);
+  //   var artistEntryURI = entrystore.getEntryURI(context, artistEntryId)
+  //   entrystore.getEntry(artistEntryURI).then(function(artistEntry) {
+  //     setGivenName(artistEntry.getMetadata().findFirstValue(props.resourceURI, "foaf:givenName"))
+  //     setFamilyName(artistEntry.getMetadata().findFirstValue(props.resourceURI, "foaf:familyName"));
+  //   });
+  // }, []);
+
   useEffect(() => {
-    var artistEntryId = es.getEntryId(props.resourceURI);
-    var artistEntryURI = es.getEntryURI(context, artistEntryId)
-    es.getEntry(artistEntryURI).then(function(artistEntry) {
-      setGivenName(artistEntry.getMetadata().findFirstValue(props.resourceURI, "foaf:givenName"))
-      setFamilyName(artistEntry.getMetadata().findFirstValue(props.resourceURI, "foaf:familyName"));
-    });
+
+    const fetchData = async () => {
+      try {
+        var artistEntryId = entrystore.getEntryId(props.resourceURI);
+        var artistEntryURI = entrystore.getEntryURI(context, artistEntryId)
+        const artistEntry = await entrystore.getEntry(artistEntryURI);
+        setGivenName(artistEntry.getMetadata().findFirstValue(props.resourceURI, "foaf:givenName"))
+        setFamilyName(artistEntry.getMetadata().findFirstValue(props.resourceURI, "foaf:familyName"));
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <div>
-        <p>Artist: {givenName} {familyName}</p>
-    </div>
+    <h6 className="card-subtitle mb-2 text-muted">{givenName} {familyName}</h6>
+    // <div>
+    //     <p>Artist: {givenName} {familyName}</p>
+    // </div>
   );
 }
 
@@ -43,37 +57,43 @@ function PieceOfArt(props) {
       </a>
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
-        {/*<p className="card-text">Artist resourceURI: {props.artist}</p>*/}
+        {/*<a href={props.artist}>*/}
         <Artist resourceURI={props.artist} />
+        {/*</a>*/}
+        {/*<p className="card-text">Artist resourceURI: {props.artist}</p>*/}
       </div>
     </div>
   );
 }
 
 
-const baseURI = "https://recruit.entryscape.net/store";
-var es = new EntryStore(baseURI);
+function App() {
+  const [listItems, setListItems] = useState();
+  const pieceOfArtType = "http://example.com/PieceOfArt";
+  const projection = {
+   title: "http://purl.org/dc/terms/title",
+   description: "http://purl.org/dc/terms/description",
+   imgSrc: "http://xmlns.com/foaf/0.1/img",
+   artist: "http://example.com/artist",
+  };
 
-const projection = {
- title: "http://purl.org/dc/terms/title",
- description: "http://purl.org/dc/terms/description",
- imgSrc: "http://xmlns.com/foaf/0.1/img",
- artist: "http://example.com/artist",
-};
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        var searchList = entrystore.newSolrQuery().context(context).rdfType(pieceOfArtType).list();
+        const results = await searchList.getEntries();
+        setListItems(results.map((result) => {
+            var proj = result.projection(projection);
+            return <PieceOfArt key={proj.title} imgSrc={proj.imgSrc} title={proj.title} artist={proj.artist} />
+          }
+        ));
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+    };
 
-const context = "1";
-var pieceOfArtType = "http://example.com/PieceOfArt";
-var searchList = es.newSolrQuery().context(context).rdfType(pieceOfArtType).list();
+    fetchData();
+  }, []);
 
-searchList.getEntries().then(function(results) {
-  const listItems = results.map((result) => {
-      var proj = result.projection(projection);
-      return <PieceOfArt key={proj.title} imgSrc={proj.imgSrc} title={proj.title} artist={proj.artist} />
-    }
-  );
-  ReactDOM.render(
-    <div className="card-group">{listItems}</div>,
-    document.getElementById('app')
-  );
-});
-
+  return <div className="card-group">{listItems}</div>
+}
